679b0d03df38d60ca7c1149d03dbafdc830f176d
Leoni <leoniantoinette12@gmail.com>
2022-12-01 17:09:54 +0700

    add callback to binotify app when approve/reject

--- src/main/java/binotify/services/SubscriptionServicesImpl.java
+++ src/main/java/binotify/services/SubscriptionServicesImpl.java
@@ -4,16 +4,18 @@ import binotify.formats.*;
 import binotify.dao.*;
 import javax.jws.*;
 import java.util.*;
+import java.net.*;
+import java.io.*;
 
 @WebService
 public class SubscriptionServicesImpl implements SubscriptionServices {
     private subscriptionDao subscriptionDao = new subscriptionDao();
 
     @Override
-    public subscription[] getSubscription(String ip, String endpoint) throws Exception{
+    public subscription[] getSubscription(String ip, String endpoint) throws Exception {
         List<subscription> list = this.subscriptionDao.getSubscription();
         subscription[] res = new subscription[list.size()];
-        for (int i=0; i<list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             subscription s = new subscription(list.get(i).getCreator_id(), list.get(i).getSubscriber_id());
             s.setStatus(list.get(i).getStatus());
             res[i] = s;
@@ -41,6 +43,36 @@ public class SubscriptionServicesImpl implements SubscriptionServices {
         boolean res = false;
         try {
             res = this.subscriptionDao.approveSubscription(subscription);
+            if (res) {
+                String url = "http://localhost:8080/binotify-app/src/php/subscription/approve.php";
+                String post_params = "creatorID=" + creator_id + "&subscriberID=" + subscriber_id;
+                URL obj = new URL(url);
+                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                con.setRequestMethod("POST");
+
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                wr.writeBytes(post_params);
+                wr.flush();
+                wr.close();
+
+                int responseCode = con.getResponseCode();
+                System.out.println("POST Response Code :: " + responseCode);
+
+                if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    // print result
+                    System.out.println(response.toString());
+                } else {
+                    System.out.println("POST request not worked");
+                }
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -49,10 +81,41 @@ public class SubscriptionServicesImpl implements SubscriptionServices {
 
     @Override
     public boolean rejectSubscription(String ip, String endpoint, int creator_id, int subscriber_id) {
+        System.out.println("rejectSubscription creator_id " + creator_id + " subscriber_id " + subscriber_id);
         subscription subscription = new subscription(creator_id, subscriber_id);
         boolean res = false;
         try {
             res = this.subscriptionDao.rejectSubscription(subscription);
+            if (res) {
+                String url = "http://localhost:8080/binotify-app/src/php/subscription/reject.php";
+                String post_params = "creatorID=" + creator_id + "&subscriberID=" + subscriber_id;
+                URL obj = new URL(url);
+                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                con.setRequestMethod("POST");
+
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                wr.writeBytes(post_params);
+                wr.flush();
+                wr.close();
+
+                int responseCode = con.getResponseCode();
+                System.out.println("POST Response Code :: " + responseCode);
+
+                if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    // print result
+                    System.out.println(response.toString());
+                } else {
+                    System.out.println("POST request not worked");
+                }
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
